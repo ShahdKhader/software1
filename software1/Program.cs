77e35142d42ebf3c15d57050107f326bcfb2f18c
ShahdKhader <shahdmahez@gmail.com>
2023-02-04 21:57:12 +0200

    this is the first commit

--- software1/Program.cs
+++ software1/Program.cs
@@ -14,6 +14,7 @@ namespace software1
         [STAThread]
         static void Main()
         {
+            //shahd
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());

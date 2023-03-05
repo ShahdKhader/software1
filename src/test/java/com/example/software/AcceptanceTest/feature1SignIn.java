57cc48dcbf51a21e098bd57ffc00e1d46dd5895d
ShahdKhader <shahdmahez@gmail.com>
2023-03-05 12:20:17 +0200

    this is the first commit in java project

---
+++ src/test/java/com/example/software/AcceptanceTest/feature1SignIn.java
@@ -0,0 +1,24 @@
+package com.example.software.AcceptanceTest;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class feature1SignIn {
+    @Given("i am trying to sign in")
+    public void iAmTryingToSignIn() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("press sign in and the name is unvalid")
+    public void pressSignInAndTheNameIsUnvalid() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("open unvalid-sign-in-screen")
+    public void openUnvalidSignInScreen() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}

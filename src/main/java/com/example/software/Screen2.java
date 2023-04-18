3a1bb9841321ac30ea7c00e70a19477987411045
noorata <nooratta17@gmail.com>
2023-04-19 00:27:11 +0300

    this is the FXML SCREENS for project

---
+++ src/main/java/com/example/software/Screen2.java
@@ -0,0 +1,16 @@
+package com.example.software;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+
+public class Screen2 {
+
+
+
+
+
+}
